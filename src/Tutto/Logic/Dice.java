f705a00ccbbaaa616ac5629c8793b89d22bd29d2
trox18 <95303754+trox18@users.noreply.github.com>
2022-11-18 00:38:46 +0100

    Dice changed

--- src/Tutto/Logic/Dice.java
+++ src/Tutto/Logic/Dice.java
@@ -1,4 +1,30 @@
 package Tutto.Logic;
 
+import java.util.Random;
+
 public class Dice {
+
+    // Global var
+    private int dValue = 0;
+    private boolean locked = false;
+
+    Random rn = new Random();
+
+    public int roll() {
+        dValue = rn.nextInt(1, 6);
+        return dValue;
+    }
+
+    public int getValue() {
+        return dValue;
+    }
+
+    public void setLocked() {
+        locked = true;
+    }
+
+    public boolean getLocked() {
+        return locked;
+    }
+
 }
