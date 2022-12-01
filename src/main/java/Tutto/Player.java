2e4150d0bc1e29cecc7ddd3d46537eb55e518095
Tushar Manekar <50165840+tusharmanekar@users.noreply.github.com>
2022-12-01 15:40:37 +0100

    Add files via upload

--- src/main/java/Tutto/Player.java
+++ src/main/java/Tutto/Player.java
@@ -1,4 +1,28 @@
 package Tutto;
 
+import java.util.ArrayList;
+
 public class Player {
+    public int Score = 0;
+    private String Name;
+
+    public Player(){
+        Name = Input.GetPlayerName();
+    }
+    public void play(Deck deck, Game game){
+        int points = 0;
+        boolean turnEnded = false;
+
+        while (!turnEnded){
+            // ui.showPlayers
+            Card card = deck.pullRandom();
+            // ui.showCard()
+
+            ArrayList<Die> dice = Dice.roll();
+
+            points += card.Handle(game, this, dice)
+
+        }
+        score += points
+    }
 }
