bc171922aa96ac8516d6fd865df3854bc5e04e08
Kavishan Srirangarasa <kavishanwin8@hotmail.com>
2022-12-03 00:15:39 +0100

    Brach coverage 70% (check)

--- src/test/java/Tutto/Logic/PrinterTest.java
+++ src/test/java/Tutto/Logic/PrinterTest.java
@@ -1,11 +1,36 @@
 package Tutto.Logic;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class PrinterTest {
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
-    @Test
-    void printTurn() {
-        // intentionally left blank
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PrinterTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    Printer printer = new Printer();
+
+    @BeforeEach
+            public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
     }
+
+    @AfterEach
+            public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+     // Test printer, source:
+    //https://stackoverflow.com/questions/1119385/junit-test-for-system-out-println
+    @Test
+    public void out() {
+        printer.printTurn(new int[]{1,2}, new int[]{5,3,2,1}, 500);
+        assertEquals("\r\n==================\r\nCurrent points: 500\r\n=== Kept Dices ===\r\n1 2 \r\n=== Rolled Dices ===\r\nValue: 5 3 2 1 \r\nIndex: 1 2 3 4 \r\n\r\n" , outContent.toString());
+    }
+
 }
