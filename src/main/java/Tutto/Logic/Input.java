5de9179bdf1010bde1fff35471cef70a569813e6
trox18 <95303754+trox18@users.noreply.github.com>
2022-12-02 16:45:25 +0100

    User input error handling + limitations implemented

--- src/main/java/Tutto/Logic/Input.java
+++ src/main/java/Tutto/Logic/Input.java
@@ -1,6 +1,8 @@
 package Tutto.Logic;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Input {
@@ -9,16 +11,46 @@ public class Input {
     public String[] getNames() {
         String line;
         String[] names;
+        boolean valid;
 
         System.out.println("Enter the player names (separated by a comma \",\"):");
-        line = scanner.nextLine(); // TODO: Error catching + check player amount
+        do {
+            line = scanner.nextLine();
 
-        // Separate all values by comma
-        names = line.split(",");
+            // Separate all values by comma
+            names = line.split(",");
 
-        // Sort names alphabetically
-        Arrays.sort(names);
+            // Limit name length to 15
+            for(int i=0;i< names.length;i++){
+                try {
+                    names[i] = names[i].substring(0, 15);
+                }catch (StringIndexOutOfBoundsException ignored){}
+            }
 
+            // Sort names alphabetically
+            Arrays.sort(names);
+
+            // Check between 2-4 Players
+            if(names.length<2||names.length>4){
+                valid=false;
+                System.out.println("You need between 2 and 4 players!\nEnter names again:");
+            }else{
+                valid=true;
+            }
+            
+            // Check duplicate names
+            if(valid) {
+                HashSet<Object> set = new HashSet<>();
+                for (String name : names) {
+                    if (!set.add(name)) {
+                        System.out.println(name + " is a duplicate name.\nEnter names again:");
+                        valid = false;
+                        break;
+                    }
+                }
+            }
+        }while(!valid);
+        
         return names;
     }
 
@@ -26,48 +58,81 @@ public class Input {
         String line;
         String[] dices;
         int size;
-        int[] res;
+        int[] res = new int[0];
+        boolean valid=true;
 
         System.out.println("Enter index of dices to keep (separated by a comma \",\"):");
-        line = scanner.nextLine();  // TODO: Error catching
+        do {
+            line = scanner.nextLine();
 
-        // Separate all values by comma
-        dices = line.split(",");
-        size = dices.length;
+            // Separate all values by comma
+            dices = line.split(",");
+            size = dices.length;
 
-        // Loop through each item in dices and save it as an integer
-        res = new int[size];
-        for (int i = 0; i < size; i++) {
-            res[i] = dicesRolled[Integer.parseInt(dices[i]) - 1];
-        }
+            try {
+                // Loop through each item in dices and save it as an integer
+                res = new int[size];
+                for (int i = 0; i < size; i++) {
+                    res[i] = dicesRolled[Integer.parseInt(dices[i]) - 1];
+                    valid=true;
+                }
+            } catch (ArrayIndexOutOfBoundsException exception) {
+                System.out.println("Enter valid indices!\nTry again:");
+                valid=false;
+            }
+        }while(!valid);
         return res;
     }
 
     public int getTarget() {
-        int target;
+        int target=6500;
+        boolean valid;
 
         System.out.println("Enter the target score (Recommended: 6500):");
-        target = scanner.nextInt(); // TODO: Default + error
-        scanner.nextLine();
-
+        do {
+            // Input mismatch error handling
+            try {
+                target = scanner.nextInt();
+                valid = true;
+            } catch (InputMismatchException exception) {
+                System.out.println("Enter a valid integer!");
+                valid = false;
+            }
+            scanner.nextLine();     // Finish line scan
+        }while(!valid);
         return target;
     }
 
     public boolean getPlayChoice(String name) {
         boolean printScore;
+        boolean valid;
+        char inp='n';
 
         System.out.println();
         System.out.println("############################################");
         System.out.println("It's the turn of " + name);
         System.out.println("Do you want to print the scoreboard?");
-        System.out.println("Y - Yes | N - No");         // TODO: Catch empty entry error
-        String inp = scanner.nextLine().toLowerCase();
-        printScore = inp.charAt(0) == 'y';
+        System.out.println("Y - Yes | N - No");
+
+        do {
+            // Catch errors
+            try {
+                inp = scanner.nextLine().toLowerCase().charAt(0);
+                valid=true;
+            } catch (StringIndexOutOfBoundsException exception) {
+                System.out.println("Invalid argument. Try again.");
+                valid=false;
+            }
+        }while(!valid);
+
+        // If inp=='y' -> true, else false
+        printScore = inp == 'y';
 
         return printScore;
     }
 
     public void close() {
+        // Close scanner at end of program
         scanner.close();
     }
 }
